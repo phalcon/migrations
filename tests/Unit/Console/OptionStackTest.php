3388dc36f6405a5cbae7bab78df5007925a8b11c
Anton Vasiliev <aionbase@gmail.com>
2019-10-19 16:29:57 +0100

    Rewrite OptionStackTest test

--- tests/Unit/Console/OptionStackTest.php
+++ tests/Unit/Console/OptionStackTest.php
@@ -1,219 +1,122 @@
 <?php
+declare(strict_types=1);
 
 namespace Phalcon\Console;
 
-use Phalcon\Test\Module\UnitTest;
-use Phalcon\Console\OptionStack;
-use Phalcon\Console\OptionParserTrait;
+use PHPUnit\Framework\TestCase;
+use Phalcon\Migrations\Console\OptionParserTrait;
+use Phalcon\Migrations\Console\OptionStack;
 
-/*
-  +------------------------------------------------------------------------+
-  | Phalcon Developer Tools                                                |
-  +------------------------------------------------------------------------+
-  | Copyright (c) 2011-2017 Phalcon Team (https://www.phalconphp.com)      |
-  +------------------------------------------------------------------------+
-  | This source file is subject to the New BSD License that is bundled     |
-  | with this package in the file LICENSE.txt.                             |
-  |                                                                        |
-  | If you did not receive a copy of the license and are unable to         |
-  | obtain it through the world-wide-web, please send an email             |
-  | to license@phalconphp.com so we can send you a copy immediately.       |
-  +------------------------------------------------------------------------+
-  | Authors: Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>             |
-  +------------------------------------------------------------------------+
-*/
-
-class OptionStackTest extends UnitTest
+final class OptionStackTest extends TestCase
 {
     use OptionParserTrait;
 
-    protected $options;
-
-    public function _before()
+    /**
+     * @var OptionStack
+     */
+    public $options;
+    
+    public function setUp(): void
     {
         $this->options = new OptionStack();
     }
 
     /**
-     * Tests OptionStack::setOptions, OptionStack::getOptions
-     *
-     * @test
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-09-28
+     * @see testSetOptionAndGetOption11
      */
-    public function shouldSetOptionsAndGetOptions()
+    public function setOptionAndGetOption11DataProvider(): array
     {
-        $this->options->setOptions(['test' => 'foo', 'test2' => 'bar']);
-
-        $this->specify(
-            "Options didn't set",
-            function($options, $expected)
-            {
-                expect($options)->equals($expected);
-            },
-            [
-                'examples' => [
-                    [$this->options->getOptions(), ['test' => 'foo', 'test2' => 'bar']]
-                ]
-            ]
-        );
+        return [
+            ['foo-bar', 'bar-foo', 'foo-bar'],
+            ['', 'bar-foo', 'bar-foo'],
+        ];
     }
 
     /**
-     * Tests OptionStack::setOption, OptionStack::getOption
-     *
-     * @test
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-09-28
+     * @see testSetDefaultOptionIfOptionDidntExist
      */
-    public function shouldSetOptionAndGetOption11()
+    public function setDefaultOptionIfOptionDidntExistDataProvider(): array
     {
-        $this->specify(
-            "Option didn't set",
-            function($value, $defaultValue, $expected)
-            {
-                $this->options->setOption('set-test', $value, $defaultValue);
+        return [
+            ['test', 'foo-bar', 'bar'],
+            ['test2', 'bar-foo', 'bar-foo'],
+        ];
+    }
 
-                expect($this->options->getOption('set-test'))->equals($expected);
-            },
-            [
-                'examples' => [
-                    ['foo-bar', 'bar-foo', 'foo-bar'],
-                    ['', 'bar-foo', 'bar-foo']
-                ]
-            ]
-        );
+    public function testGetAndSetOptions(): void
+    {
+        $options = [
+            'test' => 'foo', 
+            'test2' => 'bar',
+        ];
+        
+        $this->options->setOptions($options);
+        
+        $this->assertSame($options, $this->options->getOptions());
     }
 
     /**
-     * Tests OptionStack::setDefaultOption
+     * @dataProvider setOptionAndGetOption11DataProvider
      *
-     * @test
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-09-28
+     * @param $option
+     * @param $defaultValue
+     * @param $expected
      */
-    public function shouldSetDefaultOptionIfOptionDidntExist()
+    public function testSetOptionAndGetOption11($option, $defaultValue, $expected): void
+    {
+        $key = 'set-test';
+        $this->options->setOption($key, $option, $defaultValue);
+
+        $this->assertSame($expected, $this->options->getOption($key));
+    }
+
+    /**
+     * @dataProvider setDefaultOptionIfOptionDidntExistDataProvider
+     *
+     * @param $key
+     * @param $defaultValue
+     * @param $expected
+     */
+    public function testSetDefaultOptionIfOptionDidntExist($key, $defaultValue, $expected): void
     {
         $this->options->setOption('test', 'bar');
+        $this->options->setDefaultOption($key, $defaultValue);
 
-        $this->specify(
-            "Method setDefaultOption didn't work well",
-            function($key, $defaultValue, $expected)
-            {
-                $this->options->setDefaultOption($key, $defaultValue);
-
-                expect($this->options->getOption($key))->equals($expected);
-            },
-            [
-                'examples' => [
-                    ['test', 'foo-bar', 'bar'],
-                    ['test2', 'bar-foo', 'bar-foo']
-                ]
-            ]
-        );
+        $this->assertSame($expected, $this->options->getOption($key));
     }
 
-    /**
-     * Tests OptionStack::isReceivedOption
-     *
-     * @test
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-09-28
-     */
-    public function shouldCheckingRecievedOption()
+    public function testCheckingRecievedOption(): void
     {
         $this->options->setOption('true-option', 'foo-bar');
 
-        $this->specify(
-            "Checking option didn't wotk",
-            function($option, $expected)
-            {
-                expect($option)->equals($expected);
-            },
-            [
-                'examples' => [
-                    [$this->options->isReceivedOption('true-option', $this->options->getOptions()), true],
-                    [$this->options->isReceivedOption('false-option', $this->options->getOptions()), false]
-                ]
-            ]
-        );
+        $option1 = $this->options->isReceivedOption('true-option');
+        $option2 = $this->options->isReceivedOption('false-option');
+
+        $this->assertTrue($option1);
+        $this->assertFalse($option2);
     }
 
-    /**
-     * Tests OptionStack::getValidOption
-     *
-     * @test
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-09-28
-     */
-    public function shouldReturnValidOptionOrSetDefault()
+    public function testReturnValidOptionOrSetDefault(): void
     {
         $this->options->setOptions(['test' => 'foo', 'test2' => 'bar']);
 
-        $this->specify(
-            "Valid value didn't return",
-            function($option, $expected)
-            {
-                expect($option)->equals($expected);
-            },
-            [
-                'examples' => [
-                    [$this->options->getValidOption('test', 'bar'), 'foo'],
-                    [$this->options->getValidOption('false-option', 'bar'), 'bar'],
-                ]
-            ]
-        );
+        $this->assertSame('foo', $this->options->getValidOption('test', 'bar'));
+        $this->assertSame('bar', $this->options->getValidOption('false-option', 'bar'));
     }
 
-    /**
-     * Tests OptionParserTrait::getPrefixOption
-     *
-     * @test
-     * @issue  595
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-10-06
-     */
-    public function shouldReturnPrefixFromOptionWithoutSetPrefix()
+    public function testReturnPrefixFromOptionWithoutSetPrefix(): void
     {
         $this->options->setOptions(['test' => 'foo', 'test2' => 'bar']);
 
-        $this->specify(
-            'Method' . __METHOD__ . 'does not return option prefix',
-            function($prefix, $expected) {
-                expect($this->getPrefixOption($prefix))->equals($expected);
-            },
-            [
-                'examples' => [
-                    ['foo*', 'foo'],
-                    ['bar*', 'bar']
-                ]
-            ]
-        );
+        $this->assertSame('foo', $this->options->getPrefixOption('foo*'));
+        $this->assertSame('bar', $this->options->getPrefixOption('bar*'));
     }
 
-    /**
-     * Tests OptionParserTrait::getPrefixOption
-     *
-     * @test
-     * @issue  595
-     * @author Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>
-     * @since  2017-10-06
-     */
-    public function shouldReturnPrefixFromOptionWithSetPrefix()
+    public function testReturnPrefixFromOptionWithSetPrefix(): void
     {
         $this->options->setOptions(['test' => 'foo', 'test2' => 'bar']);
 
-        $this->specify(
-            'Method' . __METHOD__ . 'does not return option prefix',
-            function($prefix, $prefixEnd, $expected) {
-                expect($this->getPrefixOption($prefix, $prefixEnd))->equals($expected);
-            },
-            [
-                'examples' => [
-                    ['foo^', '^', 'foo'],
-                    ['bar?', '?', 'bar']
-                ]
-            ]
-        );
+        $this->assertSame('foo', $this->getPrefixOption('foo^', '^'));
+        $this->assertSame('bar', $this->getPrefixOption('bar?', '?'));
     }
 }
