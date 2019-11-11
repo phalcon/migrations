693a099006cc39f2316051c6b4b027c0547e0bec
Anton Vasiliev <aionbase@gmail.com>
2019-11-11 22:41:22 +0000

    DRY in Migrations Test + new test

--- tests/Integration/MigrationsTest.php
+++ tests/Integration/MigrationsTest.php
@@ -4,49 +4,62 @@ declare(strict_types=1);
 namespace Phalcon\Migrations\Tests\Integration;
 
 use Phalcon\Config;
+use Phalcon\Db\Column;
 use Phalcon\Migrations\Migrations;
 use function Phalcon\Migrations\Tests\root_path;
 
 final class MigrationsTest extends IntegrationTestCase
 {
+    /**
+     * @var array
+     */
+    protected static $generateConfig;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        
+        self::$generateConfig = new Config([
+            'database' => [
+                'adapter' => getenv('TEST_DB_ADAPTER'),
+                'host' => getenv('TEST_DB_HOST'),
+                'username' => getenv('TEST_DB_USER'),
+                'password' => getenv('TEST_DB_PASSWORD'),
+                'dbname' => getenv('TEST_DB_DATABASE'),
+            ],
+        ]);
+    }
+
     public function testGenerateEmptyDataBase(): void
     {
         $migrationsDir = root_path('tests/var/output/' . __FUNCTION__);
 
-        $config = new Config([
-            'database' => [
-                'adapter' => 'mysql',
-                'host' => getenv('TEST_DB_HOST'),
-                'username' => getenv('TEST_DB_USER'),
-                'password' => getenv('TEST_DB_PASSWORD'),
-                'dbname' => getenv('TEST_DB_DATABASE'),
-            ],
-        ]);
-
         $options = [
             'migrationsDir' => [
                 $migrationsDir,
             ],
-            'config' => $config,
+            'config' => self::$generateConfig,
         ];
-        
+
         Migrations::generate($options);
-        
+
         $this->assertDirectoryExists($migrationsDir);
         $this->assertSame(count(scandir($migrationsDir . '/1.0.0')), 2);
     }
     
-    public function testGenerate(): void
+    public function testGenerateSingleTable(): void
     {
         $migrationsDir = root_path('tests/var/output/' . __FUNCTION__);
-
-        $config = new Config([
-            'database' => [
-                'adapter' => 'mysql',
-                'host' => getenv('TEST_DB_HOST'),
-                'username' => getenv('TEST_DB_USER'),
-                'password' => getenv('TEST_DB_PASSWORD'),
-                'dbname' => getenv('TEST_DB_DATABASE'),
+        
+        $this->db->createTable('test', getenv('TEST_DB_DATABASE'), [
+            'columns' => [
+                new Column('column_name', [
+                    'type' => Column::TYPE_INTEGER,
+                    'size' => 10,
+                    'unsigned' => true,
+                    'notNull' => true,
+                    'first' => true,
+                ]),
             ],
         ]);
         
@@ -54,13 +67,60 @@ final class MigrationsTest extends IntegrationTestCase
             'migrationsDir' => [
                 $migrationsDir,
             ],
-            'config' => $config,
+            'config' => self::$generateConfig,
             'tableName' => '@',
         ];
         
         Migrations::generate($options);
 
         $this->assertDirectoryExists($migrationsDir);
-        $this->assertTrue(scandir($migrationsDir . '/1.0.0') > 2);
+        $this->assertSame(3, count(scandir($migrationsDir . '/1.0.0')));
+    }
+
+    public function testGenerateTwoTables(): void
+    {
+        $migrationsDir = root_path('tests/var/output/' . __FUNCTION__);
+
+        $this->db->createTable('test', getenv('TEST_DB_DATABASE'), [
+            'columns' => [
+                new Column('column_name', [
+                    'type' => Column::TYPE_INTEGER,
+                    'size' => 10,
+                    'unsigned' => true,
+                    'notNull' => true,
+                    'first' => true,
+                ]),
+                new Column('another_column', [
+                    'type' => Column::TYPE_VARCHAR,
+                    'size' => 255,
+                    'unsigned' => true,
+                    'notNull' => true,
+                ]),
+            ],
+        ]);
+
+        $this->db->createTable('test2', getenv('TEST_DB_DATABASE'), [
+            'columns' => [
+                new Column('another_column', [
+                    'type' => Column::TYPE_VARCHAR,
+                    'size' => 255,
+                    'unsigned' => true,
+                    'notNull' => true,
+                ]),
+            ],
+        ]);
+
+        $options = [
+            'migrationsDir' => [
+                $migrationsDir,
+            ],
+            'config' => self::$generateConfig,
+            'tableName' => '@',
+        ];
+
+        Migrations::generate($options);
+
+        $this->assertDirectoryExists($migrationsDir);
+        $this->assertSame(4, count(scandir($migrationsDir . '/1.0.0')));
     }
 }
