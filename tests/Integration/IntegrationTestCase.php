85cb881d29da8ee310aabd59f3fd5bd0853fe03f
Anton Vasiliev <aionbase@gmail.com>
2019-11-11 22:40:27 +0000

    Implement setting up and tearing down of database for each test

--- tests/Integration/IntegrationTestCase.php
+++ tests/Integration/IntegrationTestCase.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 
 namespace Phalcon\Migrations\Tests\Integration;
 
+use Phalcon\Db\Adapter\Pdo\AbstractPdo;
+use Phalcon\Db\Adapter\PdoFactory;
 use PHPUnit\Framework\TestCase;
 use function Phalcon\Migrations\Tests\remove_dir;
 use function Phalcon\Migrations\Tests\root_path;
 
 class IntegrationTestCase extends TestCase
 {
+    /**
+     * @var AbstractPdo
+     */
+    protected $db;
+
     public static function setUpBeforeClass(): void
     {
         ob_start();
@@ -16,11 +23,39 @@ class IntegrationTestCase extends TestCase
 
     public static function tearDownAfterClass(): void
     {
-        $output = ob_get_clean();
+        ob_get_clean();
 
         /**
          * Cleanup tests output folders
          */
         remove_dir(root_path('tests/var/output/'));
+    }
+
+    public function setUp(): void
+    {
+        $this->db = $this->initializeDatabase();
+    }
+
+    public function tearDown(): void
+    {
+        /**
+         * Cleanup Database
+         */
+        $this->db->query('DROP DATABASE IF EXISTS `' . getenv('TEST_DB_DATABASE') . '`;');
+    }
+
+    protected function initializeDatabase(): AbstractPdo
+    {
+        /** @var AbstractPdo $db */
+        $db = (new PdoFactory())->newInstance(getenv('TEST_DB_ADAPTER'), [
+            'host' => getenv('TEST_DB_HOST'),
+            'port' => getenv('TEST_DB_PORT'),
+            'username' => getenv('TEST_DB_USER'),
+            'password' => getenv('TEST_DB_PASSWORD'),
+        ]);
+
+        $db->query('CREATE DATABASE IF NOT EXISTS `' . getenv('TEST_DB_DATABASE') . '`;');
+
+        return $db;
     }
 }
