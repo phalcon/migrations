281f8582bf884b4aaa7c8ea87ee3296b571f8f4e
Anton Vasiliev <aionbase@gmail.com>
2020-02-01 20:43:06 +0000

    Implement connection with Postgres

--- tests/Integration/IntegrationTestCase.php
+++ tests/Integration/IntegrationTestCase.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Phalcon\Migrations\Tests\Integration;
 
+use Exception;
 use Phalcon\Config;
 use Phalcon\Db\Adapter\Pdo\AbstractPdo;
 use Phalcon\Db\Adapter\PdoFactory;
@@ -29,6 +30,7 @@ class IntegrationTestCase extends TestCase
             'database' => [
                 'adapter' => getenv('TEST_DB_ADAPTER'),
                 'host' => getenv('TEST_DB_HOST'),
+                'port' => getenv('TEST_DB_PORT'),
                 'username' => getenv('TEST_DB_USER'),
                 'password' => getenv('TEST_DB_PASSWORD'),
                 'dbname' => getenv('TEST_DB_DATABASE'),
@@ -51,17 +53,51 @@ class IntegrationTestCase extends TestCase
         remove_dir(root_path('tests/var/output/'));
     }
 
+    /**
+     * @throws Exception
+     */
     public function setUp(): void
     {
-        $this->db = $this->initializeDatabase();
+        $adapter = getenv('TEST_DB_ADAPTER');
+        switch ($adapter) {
+            case 'mysql':
+                $this->db = $this->initializeMySQL();
+                break;
+
+            case 'postgresql':
+                $this->db = $this->initializePostgreSQL();
+                break;
+
+            default:
+                throw new Exception(sprintf('Adapter %s not found', $adapter));
+                break;
+        }
+
     }
 
+    /**
+     * Cleanup Database and reset connection
+     *
+     * @throws Exception
+     */
     public function tearDown(): void
     {
-        /**
-         * Cleanup Database
-         */
-        $this->db->query('DROP DATABASE IF EXISTS `' . getenv('TEST_DB_DATABASE') . '`;');
+        $adapter = getenv('TEST_DB_ADAPTER');
+        $dbName = getenv('TEST_DB_DATABASE');
+
+        switch ($adapter) {
+            case 'mysql':
+                $this->db->query('DROP DATABASE IF EXISTS `' . $dbName . '`;');
+                break;
+
+            case 'postgresql':
+                $this->db->query('DROP SCHEMA IF EXISTS ' . $dbName . ' CASCADE;');
+                break;
+
+            default:
+                throw new Exception(sprintf('Adapter %s not found', $adapter));
+                break;
+        }
 
         /**
          * Reset filename or DB connection
@@ -69,7 +105,28 @@ class IntegrationTestCase extends TestCase
         Migrations::resetStorage();
     }
 
-    protected function initializeDatabase(): AbstractPdo
+    protected function initializePostgreSQL(): AbstractPdo
+    {
+        /** @var AbstractPdo $db */
+        $db = (new PdoFactory())->newInstance(getenv('TEST_DB_ADAPTER'), [
+            'host' => getenv('TEST_DB_HOST'),
+            'port' => getenv('TEST_DB_PORT'),
+            'username' => getenv('TEST_DB_USER'),
+            'password' => getenv('TEST_DB_PASSWORD'),
+        ]);
+
+        $databaseName = getenv('TEST_DB_DATABASE');
+
+        $row = $db->fetchOne('SELECT datname FROM pg_catalog.pg_database WHERE datname = \'' . $databaseName . '\';');
+        if (empty($row)) {
+            $db->query('CREATE SCHEMA "' . $databaseName . '";');
+            $db->query('SET search_path TO "' . $databaseName . '"');
+        }
+
+        return $db;
+    }
+
+    protected function initializeMySQL(): AbstractPdo
     {
         /** @var AbstractPdo $db */
         $db = (new PdoFactory())->newInstance(getenv('TEST_DB_ADAPTER'), [
