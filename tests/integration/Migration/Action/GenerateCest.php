492603928d043a520acfba87d2c2a7f99fa67398
Anton Vasiliev <aionbase@gmail.com>
2020-03-17 21:54:17 +0000

    #20 - Move GenerateCest to integration/ folder

--- tests/integration/Migration/Action/GenerateCest.php
+++ tests/integration/Migration/Action/GenerateCest.php
@@ -11,86 +11,31 @@
 
 declare(strict_types=1);
 
-namespace Phalcon\Migrations\Tests\Integration\Migration\Action;
+namespace Phalcon\Migrations\Tests\Unit\Migration\Action;
 
-use Codeception\Example;
 use IntegrationTester;
-use Phalcon\Db\Column;
 use Phalcon\Migrations\Exception\Db\UnknownColumnTypeException;
 use Phalcon\Migrations\Migration\Action\Generate;
 
 final class GenerateCest
 {
     /**
-     * @dataProvider columnsDefinitionDataProvider
-     * @param IntegrationTester $I
-     * @param Example $example
-     * @throws UnknownColumnTypeException
-     */
-    public function getColumns(IntegrationTester $I, Example $example): void
-    {
-        $I->wantToTest('Migration\Action\Generate - assert count definition');
-
-        $class = new Generate('mysql', $example['definition']);
-
-        $columns = [];
-        foreach ($class->getColumns() as $columnName => $columnDefinition) {
-            $columns[] = $columnDefinition;
-        }
-
-        $I->assertEquals($example['count'], count($columns));
-    }
-
-    /**
      * @param IntegrationTester $I
      * @throws UnknownColumnTypeException
      */
-    public function getColumnsUnsupportedTypesException(IntegrationTester $I): void
+    public function construct(IntegrationTester $I): void
     {
-        $I->expectThrowable(UnknownColumnTypeException::class, function() {
-            $class = new Generate('mysql', [
-                new Column('unknown', [
-                    'type' => 9000,
-                ]),
-            ]);
+        $I->wantToTest('Migration\Action\Generate - __construct()');
 
-            foreach ($class->getColumns() as $column) {
-                // Wait exception
-            }
-        });
-    }
+        $adapter = 'mysql';
+        $class = new Generate($adapter);
 
-    /**
-     * @return array
-     */
-    protected function columnsDefinitionDataProvider(): array
-    {
-        return [
-            [
-                'definition' => [
-                    new Column('test1', [
-                        'type' => Column::TYPE_INTEGER,
-                        'size' => 10,
-                        'primary' => true,
-                    ]),
-                ],
-                'count' => 1,
-            ],
-            [
-                'definition' => [
-                    new Column('test1', [
-                        'type' => Column::TYPE_INTEGER,
-                        'size' => 10,
-                        'primary' => true,
-                    ]),
-                    new Column('test1', [
-                        'type' => Column::TYPE_VARCHAR,
-                        'size' => 255,
-                        'notNull' => false,
-                    ]),
-                ],
-                'count' => 2,
-            ]
-        ];
+        $I->assertSame($adapter, $class->getAdapter());
+        $I->assertIsObject($class->getColumns());
+        $I->assertIsObject($class->getIndexes());
+        $I->assertIsObject($class->getReferences());
+        $I->assertIsArray($class->getOptions(false));
+        $I->assertIsArray($class->getNumericColumns());
+        $I->assertNull($class->getPrimaryColumnName());
     }
 }
